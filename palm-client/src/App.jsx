c2563cf22997e68c19cd58a6af8d008b4d80a9f7
Ronney Vargas <ronneyvargas@gmail.com>
2023-09-30 14:07:19 -0500

    Added a start and dev script

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,25 +1,47 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import loader from "./assets/loader.svg";
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+
+marked.use({
+  gfm: true,
+});
 
 function App() {
   const [serverData, setServerData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [userPrompt, setUserPrompt] = useState("");
+  const inputRef = useRef(null);
 
   const handleSubmit = () => {
+    setServerData("");
     setIsLoading(true);
-    fetch("/api", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt: userPrompt }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setServerData(data);
-        setIsLoading(false);
-      });
+
+    if (userPrompt !== "") {
+      fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: userPrompt }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const html = DOMPurify.sanitize(marked(data));
+          setServerData({ ...data, html });
+          inputRef.current.focus();
+          setUserPrompt("");
+          setIsLoading(false);
+        });
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+    }
   };
 
   return (
@@ -33,31 +55,29 @@ function App() {
       }}
     >
       <h1 style={{ padding: "10px", marginBottom: "0px" }}>Text Genie</h1>
-      <div style={{ margin: "0", flexGrow: "1" }}>
-        <div style={{ width: "100%", height: "100%" }}>
+      <div style={{ margin: "0", flexGrow: "1", overflow: "auto" }}>
+        <div style={{ width: "100%", height: "auto" }}>
           {isLoading ? (
             <img src={loader} alt="loading" />
+          ) : !serverData ? (
+            <div style={{ margin: "0px 15px" }}>
+              <h2>Welcome to Text Genie!</h2>
+              <p>To get started:</p>
+              <ol>
+                <li>
+                  Type your prompt in the text area at the bottom of the screen.
+                </li>
+                <li>
+                  Click the &apos;Go&apos; button to generate your article.
+                </li>
+              </ol>
+              <p>Enjoy using the app!</p>
+            </div>
           ) : (
-            <article style={{ margin: "0" }}>
-              {!serverData ? (
-                <div>
-                  <h2>Welcome to Text Genie!</h2>
-                  <p>To get started:</p>
-                  <ol>
-                    <li>
-                      Type your prompt in the text area at the bottom of the
-                      screen.
-                    </li>
-                    <li>
-                      Click the &apos;Go&apos; button to generate your article.
-                    </li>
-                  </ol>
-                  <p>Enjoy using the app!</p>
-                </div>
-              ) : (
-                serverData
-              )}
-            </article>
+            <article
+              style={{ margin: "0" }}
+              dangerouslySetInnerHTML={{ __html: serverData.html }}
+            ></article>
           )}
         </div>
       </div>
@@ -73,6 +93,9 @@ function App() {
           onChange={(e) => setUserPrompt(e.target.value)}
           style={{ margin: "0", flexGrow: "1", overflowY: "hidden" }}
           placeholder="Type in Prompt"
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+          value={userPrompt}
         />
         <button
           onClick={handleSubmit}
